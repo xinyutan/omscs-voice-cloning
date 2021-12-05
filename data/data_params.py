602add1a797810ad3ae672e28df42c2cc5a075e5
xinyutan <xinyutan@umich.edu>
2021-12-04 21:07:15 -0800

    gcp parameter

--- data/data_params.py
+++ data/data_params.py
@@ -7,7 +7,7 @@ mel_n_channels = 40
 ## Audio
 sampling_rate = 16000
 # Number of spectrogram frames in a partial utterance
-partials_n_frames = 160     # 1600 ms
+partials_n_frames = 200     # 1600 ms
 # Number of spectrogram frames at inference
 inference_n_frames = 80     #  800 ms
 
--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from data.dataset import *
 
 if __name__ == '__main__':
     
-    train_dataset = 
+    train_dataset = SpeakerVerifierDataset(Path('/home/tanxy_nju/omscs-voice-cloning/SV2TTS/encoder/'))
     num_epochs = 100_000_000
     model_dir = Path('saved_models')
-    train('120421', train_dataset, dev_dataset, num_epochs, model_dir, 2000, 2000)
+    train('120421', train_dataset, train_dataset, \
+        num_epochs, model_dir, 2000, 2000)
